ae347a0c3af6269e1bc8785d76c52131498230f5
Artem <artem.rmb@bk.ru>
2022-12-06 23:19:30 +0300

    Add button for creating

--- Qt/loginMenu.py
+++ Qt/loginMenu.py
@@ -18,6 +18,9 @@ class LoginWindow(QMainWindow):
 
         self.enterToSys = QPushButton()
         self.enterToSys.setText("Enter")
+        
+        self.createEmployee = QPushButton()
+        self.createEmployee.setText("Create")
 
         self.combobox = QComboBox()
         self.combobox.addItems(['Director', 'Administrator', 'Waiter'])
@@ -26,6 +29,7 @@ class LoginWindow(QMainWindow):
         self.gridLayout.addWidget(self.lineEditLogin, 0, 1, 1, 1)
         self.gridLayout.addWidget(self.lineEditPassword, 0, 2, 1, 1)
         self.gridLayout.addWidget(self.enterToSys, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.createEmployee, 2, 1, 1, 1)
         
         self.centralwidget.setLayout(self.gridLayout)
         self.setCentralWidget(self.centralwidget)
