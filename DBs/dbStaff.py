153021595acb25221d8a32702100266f8df043ff
Artem <artem.rmb@bk.ru>
2022-12-07 01:22:07 +0300

    Create function

--- DBs/dbStaff.py
+++ DBs/dbStaff.py
@@ -1,16 +1,17 @@
 import sqlite3
 
+def createStaff():
+    con = sqlite3.connect('staff.db')
+    cur = con.cursor()
+    
+    cur.execute("""CREATE TABLE IF NOT EXISTS staff(
+        post TEXT
+        id INT PRIMARY KEY UNIQUE,
+        password TEXT);
+        """)
+    con.commit()
 
-con = sqlite3.connect('staff.db')
-cur = con.cursor()
 
-cur.execute("""CREATE TABLE IF NOT EXISTS staff(
-    post TEXT,
-    login_id INT PRIMARY KEY UNIQUE,
-    password INT);
-    """)
-con.commit()
-
-
-staff = [('director', '777', '555'), ('administator', '111', '222'), ('waiter', '001', '001')]
-cur.executemany("INSERT INTO staff VALUES(?, ?, ?);", staff)
+    staff = [('director', '777', '555'), ('adminstrator', '111', '222'), ('waiter', '001', '001')]
+    cur.executemany("INSERT OR REPLACE INTO staff VALUES(?, ?, ?);", staff)
+    con.commit()
