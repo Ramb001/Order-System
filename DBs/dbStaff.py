2a783d78d44aeb1e8114c2d83262f00146f72201
Artem <artem.rmb@bk.ru>
2022-12-07 02:56:39 +0300

    Changed struct of file

--- DBs/dbStaff.py
+++ DBs/dbStaff.py
@@ -1,16 +1,15 @@
 import sqlite3
 
+con = sqlite3.connect('staff.db')
+cur = con.cursor()
+
 def createStaff():
-    con = sqlite3.connect('staff.db')
-    cur = con.cursor()
-    
     cur.execute("""CREATE TABLE IF NOT EXISTS staff(
-        post TEXT
-        id INT PRIMARY KEY UNIQUE,
+        post TEXT,
+        id TEXT PRIMARY KEY UNIQUE,
         password TEXT);
         """)
     con.commit()
-
 
     staff = [('director', '777', '555'), ('adminstrator', '111', '222'), ('waiter', '001', '001')]
     cur.executemany("INSERT OR REPLACE INTO staff VALUES(?, ?, ?);", staff)
