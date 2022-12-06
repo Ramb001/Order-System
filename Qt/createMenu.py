73251546c60c6224f689f9cf064c5061b71ebd6c
Artem <artem.rmb@bk.ru>
2022-12-07 02:55:33 +0300

    Create save new personal

--- Qt/createMenu.py
+++ Qt/createMenu.py
@@ -1,7 +1,9 @@
-from PyQt5.QtWidgets import QLineEdit, QWidget, QGridLayout, QPushButton, QMainWindow, QComboBox
+from PyQt5.QtWidgets import QLineEdit, QWidget, QGridLayout, QPushButton, QMainWindow, QComboBox, QBoxLayout
+
+from handlers.addStaff import add
+
 
 class CreateWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
         
@@ -21,8 +23,8 @@ class CreateWindow(QMainWindow):
 
         self.createEmployee = QPushButton()
         self.createEmployee.setText("Create")
-        self.combobox.currentIndexChanged.connect(self.currentPost)
-        self.createEmployee.clicked.connect(self.currentPost)
+        self.combobox.currentIndexChanged.connect(self.addNewUser)
+        self.createEmployee.clicked.connect(self.addNewUser)
         
         self.gridLayout.addWidget(self.combobox, 0, 0, 1, 1)
         self.gridLayout.addWidget(self.lineEditLogin, 0, 1, 1, 1)
@@ -31,8 +33,12 @@ class CreateWindow(QMainWindow):
         
         self.centralwidget.setLayout(self.gridLayout)
         self.setCentralWidget(self.centralwidget)
-        
     
-    def currentPost(self, index):
-        post = self.items[index]
-        print(post)
+    def addNewUser(self):
+        post = str(self.combobox.currentText()).lower()
+        login = str(self.lineEditLogin.text())
+        password =  str(self.lineEditPassword.text())
+        if len(login) != 0 and len(password) != 0:
+            add(post, login, password)
+            login = ''
+            password = ''
