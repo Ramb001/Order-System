970dcb5898b9304b7a6578944f16796b494f09ae
Artem <artem.rmb@bk.ru>
2022-12-06 23:32:55 +0300

    Add default values for db

--- DBs/dbStaff.py
+++ DBs/dbStaff.py
@@ -6,7 +6,11 @@ cur = con.cursor()
 
 cur.execute("""CREATE TABLE IF NOT EXISTS staff(
     post TEXT,
-    login_id INT,
+    login_id INT PRIMARY KEY UNIQUE,
     password INT);
     """)
 con.commit()
+
+
+staff = [('director', '777', '555'), ('administator', '111', '222'), ('waiter', '001', '001')]
+cur.executemany("INSERT INTO staff VALUES(?, ?, ?);", staff)
