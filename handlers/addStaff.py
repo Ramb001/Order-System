ba15f7ee80c47e1177f155d772bd660c7d956086
Artem <artem.rmb@bk.ru>
2022-12-07 02:54:56 +0300

    Add folder with actions

---
+++ handlers/addStaff.py
@@ -0,0 +1,7 @@
+from DBs import dbStaff
+
+
+def add(post, login, password):
+    newUser = (post, login, password)
+    dbStaff.cur.execute("INSERT OR REPLACE INTO staff VALUES(?, ?, ?);", newUser)
+    dbStaff.con.commit()
