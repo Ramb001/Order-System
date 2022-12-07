51de7a6c19fe1ffb778c62debb3a4fdc5a92a6f3
Artem <artem.rmb@bk.ru>
2022-12-07 20:38:25 +0300

    Bug fixed

--- DB/dbStaff.py
+++ DB/dbStaff.py
@@ -6,11 +6,11 @@ cur = con.cursor()
 def createStaff():
     cur.execute("""CREATE TABLE IF NOT EXISTS staff(
         post TEXT,
-        id TEXT PRIMARY KEY UNIQUE,
+        id INT PRIMARY KEY UNIQUE,
         password TEXT);
         """)
     con.commit()
 
-    staff = [('director', '777', '555'), ('adminstrator', '111', '222'), ('waiter', '001', '001')]
+    staff = [('director', '777', '555'), ('administrator', '431', '123'), ('waiter', '001', '001')]
     cur.executemany("INSERT OR REPLACE INTO staff VALUES(?, ?, ?);", staff)
     con.commit()
