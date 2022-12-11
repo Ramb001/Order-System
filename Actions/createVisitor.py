62a4a2daf8cb6ce68429b39aad9fd3e79704f630
Artem <artem.rmb@bk.ru>
2022-12-12 01:06:34 +0300

    _

--- Actions/createVisitor.py
+++ Actions/createVisitor.py
@@ -1,8 +1,7 @@
 from DB import dbVisitors
 
 
-def createPerson(surname, name):
-    person = (surname, name, '0')
-    res = dbVisitors.cur.execute("SELECT * FROM visitors WHERE post = ? and id = ? and password = ?", (person[0], person[1], person[2],))
-    if res.fetchone() is not None:
-        return True
+def add(surname, name):
+    newUser = (surname, name, '0')
+    dbVisitors.cur3.execute("INSERT OR REPLACE INTO visitors VALUES(?, ?, ?);", newUser)
+    dbVisitors.con3.commit()
