5fda10a5e172a6c952691149791777bcd52b5732
Artem <artem.rmb@bk.ru>
2022-12-11 23:07:46 +0300

    Add function for creation visitors's db

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from DB.dbStaff import createStaff
 from DB.dbMenu import createMenu
+from DB.dbVisitors import createVisitors
 from Qt.start import run
 
 if __name__ == '__main__':
     createStaff()
     createMenu()
+    createVisitors()
     run()
