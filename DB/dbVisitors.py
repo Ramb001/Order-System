b37822ba646fb8bc039f675f90d2c5c3d4e54688
Artem <artem.rmb@bk.ru>
2022-12-12 01:06:55 +0300

    _

--- DB/dbVisitors.py
+++ DB/dbVisitors.py
@@ -5,12 +5,9 @@ con3 = sqlite3.connect('visitors.db')
 cur3 = con3.cursor()
 
 def createVisitors():
-    cur3.execute('''CREATE TABLE IF NOT EXISTS visitors(
+    cur3.execute("""CREATE TABLE IF NOT EXISTS visitors(
         surname TEXT,
         name TEXT,
-        money INT)''')
-    con3.commit()
-    
-    visitor = [('Stanko', 'Artem', '0'), ('Ustinov', 'Iliy', '0')]
-    cur3.executemany("INSERT OR REPLACE INTO visitors VALUES(?, ?, ?);", visitor)
+        money INT);
+        """)
     con3.commit()
