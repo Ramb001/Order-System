3fb5a39e771221dc41f70e8c9c9fee6e66fa4d58
Artem <artem.rmb@bk.ru>
2022-12-12 00:07:56 +0300

    _

--- Qt/visitorsMenu.py
+++ Qt/visitorsMenu.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout, QTableWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QWidget, QGridLayout, QTableWidget, QApplication, QPushButton, QTableWidgetItem
+
+from DB.dbVisitors import cur3
 
 
 class VisitorsMenu(QMainWindow):
@@ -13,6 +15,13 @@ class VisitorsMenu(QMainWindow):
         self.table.setColumnCount(len(self.columns))
         self.table.setHorizontalHeaderLabels(self.columns)
         self.table.resizeColumnsToContents()
+        cur3.execute('''SELECT * FROM visitors''')
+        self.table.setRowCount(0)
+        for row, form in enumerate(cur3):
+            self.table.insertRow(row)
+            for column, item in enumerate(form):
+                print(str(item))
+                self.table.setItem(row, column, QTableWidgetItem(str(item)))
         
         self.createButton = QPushButton()
         self.createButton.setText("Create")
@@ -22,6 +31,7 @@ class VisitorsMenu(QMainWindow):
         
         self.centralwidget.setLayout(self.gridLayout)
         self.setCentralWidget(self.centralwidget)
+        
         
 if __name__ == "__main__":
     app = QApplication([""])
