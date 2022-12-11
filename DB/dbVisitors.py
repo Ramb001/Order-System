f7a1c86c81e76b9fb679c891afde0966228cfefb
Artem <artem.rmb@bk.ru>
2022-12-11 23:06:54 +0300

    Create db for visitors

---
+++ DB/dbVisitors.py
@@ -0,0 +1,13 @@
+from distutils.util import copydir_run_2to3
+import sqlite3 
+
+
+con3 = sqlite3.connect('visitors.db')
+cur3 = con3.cursor()
+
+def createVisitors():
+    cur3.execute('''CREATE TABLE IF NOT EXISTS visitors(
+        surname TEXT,
+        name TEXT,
+        money INT)''')
+    con3.commit()
